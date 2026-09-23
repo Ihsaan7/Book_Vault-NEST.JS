dd000c18de649e5a86e987bca44ef7d808d96ed9
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-23 21:05:43 +0500

    Configiured Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from './common/filters/allExceptions.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform:true
+    })
+  )
+
+  app.useGlobalFilters(new AllExceptionFilter())
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('🚀 Server running on http://localhost:3000');
 }
-await bootstrap();
+ bootstrap();
