4a118e9ea3ee514c8ce1a36892057197ff378e0e
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-24 07:31:48 +0500

    Added module to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { DatabaseService } from './db/database.service.js';
+import { DatabaseModule } from './db/database.module.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule , AuthModule],
   controllers: [AppController],
   providers: [AppService , DatabaseService],
   exports: [DatabaseService]
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
-
+import { DatabaseModule } from '../db/database.module.js';
 
 @Module({
     controllers: [AuthController],
