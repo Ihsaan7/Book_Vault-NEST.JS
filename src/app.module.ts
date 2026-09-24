441c9b225cfe0c793480763f1293800fce2c572d
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-24 12:07:01 +0500

    App module updated with Book / Borrow module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service.js';
 import { DatabaseService } from './db/database.service.js';
 import { DatabaseModule } from './db/database.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { BorrowModule } from './borrows/borrow.module.js';
+import { BookModule } from './books/book.module.js';
 
 @Module({
-  imports: [DatabaseModule , AuthModule],
+  imports: [DatabaseModule , AuthModule , BorrowModule , BookModule],
   controllers: [AppController],
   providers: [AppService , DatabaseService],
   exports: [DatabaseService]
