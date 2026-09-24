180a933b3cbcc0e25577cb2ec8f36f696487eedc
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-24 07:14:30 +0500

    jwt erorr fixed

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -4,7 +4,7 @@ import {
     Injectable,
     UnauthorizedException,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate{
@@ -12,15 +12,29 @@ export class AuthGuard implements CanActivate{
         const request = context.switchToHttp().getRequest();
         const authHeader = request.headers['authorization']
     
-        if(!authHeader || !authHeader.startWith('Bearer '))
-            {
-                throw new UnauthorizedException("Missing or Invalid authorization header")
-            }
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
+            throw new UnauthorizedException("Missing or Invalid authorization header")
+        }
             
         const token = authHeader.split(' ')[1];
-        if(!token){ throw new UnauthorizedException('Token not provided')}
+        
+        try {
+            // Verify the JWT
+            const decoded = jwt.verify(
+                token,
+                process.env.JWT_SECRET_KEY as string || 'fallback_secret_key_for_development', // Provide fallback secret
+            ) as any;
 
-        request.user = {id:1 , role: 'USER'}
-        return true
+            // Attach user info with request
+            request.user={
+                id:decoded.id,
+                email:decoded.email,
+                role:decoded.role
+            }
+
+            return true
+        } catch (error) {
+                throw new UnauthorizedException('Invalid or expired token');
+        }
     }
 }
